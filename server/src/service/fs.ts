49958e45138a45663bb8702ce0cf1873ba2e16fd
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-04-15 22:22:09 +0800

    Service 改名。

--- server/src/service/fs.ts
+++ server/src/service/fs.ts
@@ -3,7 +3,6 @@ import { ServiceContext } from '../types';
 import { db as connections } from '../common/database';
 import { VideoFS } from '../videofs/video_fs';
 import { VideoFile } from '../videofs/video_file';
-import { FFMpeg } from '../ffmpeg';
 
 const db = connections.default;
 export class FS {
@@ -29,39 +28,8 @@ export class FS {
             })
         };
     }
-
-    public static async metadata(ctx: ServiceContext) {
-        const { videoRoot } = ctx;
-        const { video } = ctx.query;
-
-        const vfs = new VideoFS(videoRoot);
-        const vod = await VideoFile.fromFile(vfs, video);
-
-        const ffmpeg = new FFMpeg(vod.absolutePath);
-
-        const metadata = await ffmpeg.getMetadata();
-        ctx.body = metadata;
-    }
-
-    public static async screenshot(ctx: ServiceContext) {
-        const { videoRoot } = ctx;
-        const { video, seconds } = ctx.query;
-
-        const v = video;
-        const vfs = new VideoFS(videoRoot);
-        const vod = await VideoFile.fromFile(vfs, v);
-
-        const ffmpeg = new FFMpeg(vod.absolutePath);
-
-        const success = await ffmpeg.takeScreenshot(seconds);
-        ctx.body = {
-            status: success
-        };
-    }
 }
 
 export default new Router()
     .get('/fs/list', FS.list)
-    .get('/fs/metadata', FS.metadata)
-    .get('/fs/screenshot', FS.screenshot)
     ;
--- server/src/service/video.ts
+++ server/src/service/video.ts
@@ -1,9 +1,42 @@
 import Router from 'koa-router';
+import { VideoFS } from '../videofs/video_fs';
+import { VideoFile } from '../videofs/video_file';
+import { FFMpeg } from '../ffmpeg';
+import { ServiceContext } from '../types';
 
 export class Video {
+    public static async metadata(ctx: ServiceContext) {
+        const { videoRoot } = ctx;
+        const { video } = ctx.query;
+
+        const vfs = new VideoFS(videoRoot);
+        const vod = await VideoFile.fromFile(vfs, video);
+
+        const ffmpeg = new FFMpeg(vod.absolutePath);
+
+        const metadata = await ffmpeg.getMetadata();
+        ctx.body = metadata;
+    }
+
+    public static async screenshot(ctx: ServiceContext) {
+        const { videoRoot } = ctx;
+        const { video, seconds } = ctx.query;
+
+        const v = video;
+        const vfs = new VideoFS(videoRoot);
+        const vod = await VideoFile.fromFile(vfs, v);
+
+        const ffmpeg = new FFMpeg(vod.absolutePath);
+
+        const success = await ffmpeg.takeScreenshot(seconds);
+        ctx.body = {
+            status: success
+        };
+    }
 }
 
 
 export default new Router()
-// .get('/fs/list', FS.list)
+.get('/video/metadata', Video.metadata)
+.get('/video/screenshot', Video.screenshot)
 ;
