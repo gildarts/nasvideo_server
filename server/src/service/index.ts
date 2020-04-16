a24998c4cf15367a6421ee2e4fda10eb67f3be30
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-04-16 22:56:46 +0800

    刪除不需要的程式。

--- server/src/service/index.ts
+++ server/src/service/index.ts
@@ -1,10 +1,8 @@
 import Router from 'koa-router';
-import acl from './acl';
 import fs from './fs';
 import video from './video';
 
 export default new Router()
-    .use(acl.routes())
     .use(fs.routes())
     .use(video.routes())
     ;
